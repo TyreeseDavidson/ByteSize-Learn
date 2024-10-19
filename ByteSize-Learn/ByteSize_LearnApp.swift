160aa9c7b636a0672a0b14c614312dbf0b26fdb1
elipeter <elicpeter@gmail.com>
2024-10-19 12:40:43 -0400

    Initial App Push

---
+++ ByteSize-Learn/ByteSize_LearnApp.swift
@@ -0,0 +1,17 @@
+//
+//  ByteSize_LearnApp.swift
+//  ByteSize-Learn
+//
+//  Created by Eli Peter on 10/19/24.
+//
+
+import SwiftUI
+
+@main
+struct ByteSize_LearnApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ByteSize-Learn/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ByteSize-Learn
+//
+//  Created by Eli Peter on 10/19/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
