b77a589d9d0d670dd9e9deedbce03d41520b4351
elipeter <elicpeter@gmail.com>
2024-10-19 13:18:50 -0400

    Added AWS Amplify

--- ByteSize-Learn/ByteSize_LearnApp.swift
+++ ByteSize-Learn/ByteSize_LearnApp.swift
@@ -6,9 +6,28 @@
 //
 
 import SwiftUI
+import Amplify
+import AWSCognitoAuthPlugin
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+      func configureAmplify() {
+          do {
+              try Amplify.add(plugin: AWSCognitoAuthPlugin())
+              try Amplify.configure()
+              print("Amplify configured with Auth plugin")
+          } catch {
+              print("Failed to initialize Amplify with \(error)")
+          }
+      }
+    return true
+  }
+}
 
 @main
 struct ByteSize_LearnApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
     var body: some Scene {
         WindowGroup {
             ContentView()
